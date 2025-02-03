7f2448cfb80744dfdcc5c6d1aa59e9365ba0754a
Kasyap Girijan <104567271+kasyapgirijan@users.noreply.github.com>
2025-02-03 21:55:42 +0530

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738565905',resources: [
+    cacheName: 'chirpy-1738599891',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
